154adf08ab0b39d256c96f6a2300f3ffda518259
Rich Smith <rs6869@me.com>
2018-05-15 14:13:57 -0400

    Added Intro to JS Folder
    Added "js-intro-to-js-exercises" folder from Josh T @ Tech Elevator

---
+++ misc/js-intro-to-js-exercises/exercises.js
@@ -0,0 +1,20 @@
+﻿
+/**
+ * Returns the sum of x + y unless x == y then its double the sum.
+ * @param {Number} x the first number to add 
+ * @param {Number} y the second number to add 
+ */
+function sumDouble(x, y) {
+
+    let answer;
+
+
+    if (x === y) { 
+        answer = (x + y) * 2;
+    } else {
+        answer = x + y;
+    }
+
+
+    return answer;
+}
