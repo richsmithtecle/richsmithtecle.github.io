874e3ce7cca5797a3ff275291c9e457eb814914f
Rich Smith <rs6869@me.com>
2019-08-12 16:25:28 -0400

    CS React Course: Sec 5 (Quick save )

--- react-course/05-pokedex-project/pokedex/src/Pokecard.js
+++ react-course/05-pokedex-project/pokedex/src/Pokecard.js
@@ -7,14 +7,14 @@ let padToThree = (number) => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 class Pokecard extends Component {
     render() {
-        const { id, name, type, base_experience } = this.props;
+        const { id, name, type, exp } = this.props;
         let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`
         return (
             <div className="Pokecard Pokecard-grow">
                 <h1 className="Pokecard-title">{name}</h1>
                 <img src={imgSrc} alt={name}/>
                 <div className="Pokecard-data">Type: {type}</div>
-                <div className="Pokecard-data">Exp: {base_experience}</div>
+                <div className="Pokecard-data">Exp: {exp}</div>
                 
             </div>
         );
--- react-course/05-pokedex-project/pokedex/src/Pokedex.js
+++ react-course/05-pokedex-project/pokedex/src/Pokedex.js
@@ -12,21 +12,23 @@ console.log(leftFillNum(65));
 
 class Pokedex extends Component {
     static defaultProps = {
-    pokemon : [
-        {id: 4, name: 'Charmander', type: 'fire', base_experience: 62},
-        {id: 7, name: 'Squirtle', type: 'water', base_experience: 63},
-        {id: 11, name: 'Metapod', type: 'bug', base_experience: 72},
-        {id: 12, name: 'Butterfree', type: 'flying', base_experience: 178},
-        {id: 25, name: 'Pikachu', type: 'electric', base_experience: 112},
-        {id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95},
-        {id: 94, name: 'Gengar', type: 'poison', base_experience: 225},
-        {id: 133, name: 'Eevee', type: 'normal', base_experience: 65}
-      ]
+        pokemon: [
+            { id: 4, name: 'Charmander', type: 'fire', base_experience: 62 },
+            { id: 7, name: 'Squirtle', type: 'water', base_experience: 63 },
+            { id: 11, name: 'Metapod', type: 'bug', base_experience: 72 },
+            { id: 12, name: 'Butterfree', type: 'flying', base_experience: 178 },
+            { id: 25, name: 'Pikachu', type: 'electric', base_experience: 112 },
+            { id: 39, name: 'Jigglypuff', type: 'normal', base_experience: 95 },
+            { id: 94, name: 'Gengar', type: 'poison', base_experience: 225 },
+            { id: 133, name: 'Eevee', type: 'normal', base_experience: 65 }
+        ]
     };
     render() {
         return (
             <div className="Pokedex">
                 <h1 className="Pokedex-title">Pokedex</h1>
+                <p>Experience Total = {this.props.exp}</p>
+                <p>{this.props.isWinner ? "WINNER!" : 'LOSER!'}</p>
                 <div className="Pokedex-cards">
                     {/* 
                     //Original solution to enhanced images bonus(refactored and moved to Pokecard):
@@ -35,7 +37,7 @@ class Pokedex extends Component {
                     ))}
                     */}
                     {this.props.pokemon.map((p) => (
-                        <Pokecard id={p.id} name={p.name} type={p.type} base_experience={p.base_experience} />
+                        <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
                     ))}
                 </div>
             </div>
