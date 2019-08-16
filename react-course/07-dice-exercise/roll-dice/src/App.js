e12e7d81e7ca80e38f825f063f233ea6e122ec80
Rich Smith <rs6869@me.com>
2019-08-15 20:34:55 -0400

    CS React Course: Section 7 (First Commit)

---
+++ react-course/07-dice-exercise/roll-dice/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Die from './Die';
+import RollDice from './RollDice';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <RollDice />
+    </div>
+  );
+}
+
+export default App;
