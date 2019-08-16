15cc119d74da01779dddbf8a092a9cd5e30e96ef
Rich Smith <rs6869@me.com>
2019-08-16 10:05:33 -0400

    CS React Course: Section 7 (Final push)

--- react-course/07-dice-exercise/roll-dice/src/App.js
+++ react-course/07-dice-exercise/roll-dice/src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import Die from './Die';
 import RollDice from './RollDice';
-import Die2 from './Die2';
-import RollDice2 from './RollDice2';
+// import Die2 from './Die2';
+// import RollDice2 from './RollDice2';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <RollDice2 />
+      <RollDice />
     </div>
   );
 }
--- react-course/07-dice-exercise/roll-dice/src/Die.js
+++ react-course/07-dice-exercise/roll-dice/src/Die.js
@@ -5,7 +5,7 @@ class Die extends Component {
     render() {
         return (
             <div className="Die">
-                <i className={`fas fa-dice-${this.props.face} fa-5x`}></i>
+                <i className={`fas fa-dice-${this.props.face} fa-5x ${this.props.rolling ? 'shaking' : ''} `}></i>
             </div>
         );
     }
