5ea99855440fb6b0a3114823c92dbd88d413a74b
Rich Smith <rs6869@richsmacbookair.attlocal.net>
2019-08-18 11:35:29 -0400

    CS React Course: Section 8 (final)

--- react-course/08-react-state-patterns/react-state-pattern-samples/src/App.js
+++ react-course/08-react-state-patterns/react-state-pattern-samples/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import IconList from './IconList';
+// import IconList from './IconList';
+import Ball from './Ball';
+import Lottery from './Lottery';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <IconList />
+      <Lottery />
+      <Lottery title='Pick Four' numBalls={4} maxNum={10}/>
     </div>
+    // <div className="App">
+    //   <IconList />
+    // </div>
   );
 }
 
